039e6f18ce5b7c07eae496fa3d3d08e03e03701d
marvincheruiyot <marvin.cheruiyot@strathmore.edu>
2024-05-28 15:47:55 +0300

    made some changes

--- loops.php
+++ loops.php
@@ -1,51 +1,61 @@
-<h1>loops</h1>
+<h1>Loops</h1>
+<h2>while loops</h2>
 <?php
-//while loop
-
+// while loops 
 $in = 0;
-while ($in < 10){
+while($in < 10){
+
     print $in . "<br>";
     $in++;
 }
 ?>
-<h4> do-while-loop</h4>
+<h3>do-while loops</h3>
 <?php
-//do-while loop
+//do while loops
 $s = 5;
+
 do{
     print $s . "<br>";
     $s++;
 }while($s < 15);
-?>
-<h4>for loop</h4>
-<?php
-for(4=45; $f<55; $f++){
-print $f . "<br>";
-}
-?>
-<h4>Foreach</h4>
-<?php
-$months = ["January", "February" , "March"];
 
-foreach($months as $month){
-    print $month . "<br>";
+?>
+<h4>for loops</h4>
+
+<?php
+for ($f=45; $f<55; $f++ ){
+    print $f . "<br>";
 }
 ?>
+
+<h4>for-each</h4>
+
+<?php
+$months = ["january","february","march","april","may","june","july","august","september","october","november","december"];
+
+foreach($months AS $months){
+    print $months . "<br>";
+}
+?>
+
 <form action="">
-<select name="" id="">
-    <option value="">--Months--</option>
-    <?php
-    foreach($months as $month){
-    print "<option value=''>$months</option>";
-    }
-    ?>
-<select name="" id="">
-    <option value="">--Year--</option>
-    <?php
-    //while-loop
-    $y = 2014;
-    while($y < 2030){
-        print "<option value=''>$y</option>";
-        $y++;
-    }
-    ?>
+    <select name="" id="">
+        <option value="">--Months--</option>
+        <?php
+        foreach($months AS $months){
+            print "<option value=''></option>";
+        }
+        ?>
+    </select>
+
+    <select name="" id="">
+        <option value="">--Year--</option>
+        <?php
+        $y = 2014;
+        while($y < 2030){
+            print"<option value =''>$y</option>";
+            $y++;
+        }
+        ?>
+    </select>
+</form>
